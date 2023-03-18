d969b13ccc77345afc814bb8c9b3a1a90612b08b
Prylyepko Konstantin <126470871+KonstantinPryliepko@users.noreply.github.com>
2023-03-18 14:25:05 +0200

    My home work 4 - Basic data types and operations. Part 2

--- Basic_data_types_and_operations_Part_2.py
+++ Basic_data_types_and_operations_Part_2.py
@@ -1,11 +1,11 @@
 # Required task
 input_something = input("Input something - ")
-if input_something.isdigit() == True:
+if input_something.isdigit():
     if int(input_something) % 2 == 0:
         print(f" The {input_something} is digit. And the number is even.")
     else:
         print(f" The {input_something} is digit. And the number is odd.")
-elif input_something.isalpha() == True:
+elif input_something.isalpha():
     print(f" The {input_something} is word, its length is {len(input_something)}.")
 else:
     print("Error")
