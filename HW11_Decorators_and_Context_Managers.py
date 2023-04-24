4f6ba4558c45db3672122aaf6e15c04f40ce43a5
Prylyepko Konstantin <126470871+KonstantinPryliepko@users.noreply.github.com>
2023-04-24 21:32:57 +0300

    Update My home work #11. Decorators and context managers.

--- HW11_Decorators_and_Context_Managers.py
+++ HW11_Decorators_and_Context_Managers.py
@@ -16,20 +16,21 @@ def func_name_and_time(vremya):
 func_name_and_time(time.asctime())
 
 
-
 # Optional task 2
-def my_own_deco(func):
-    def wrapper(*args, **kwargs):
-        n = int(args[0])
-        while n != 0:
-            func(*args, **kwargs)
-            t = time.asctime()
-            print(f" {n}. The name of the function - {func.__name__}.\nTime when function was called {t}.")
-            n -= 1
+def my_own_deco(my_varaible):
+    def wrapper(func):
+        def inner(*args, **kwargs):
+            print(f"The name is {my_varaible}")
+            n = int(my_varaible)
+            while n != 0:
+                t = time.asctime()
+                print(f" {n}. The name of the function - {func.__name__}.\nTime when function was called {t}.")
+                n -= 1
+        return inner
     return wrapper
 
 
-@my_own_deco
-def func_name_and_time(vremya):
+@my_own_deco(my_varaible="5")
+def func_name_and_time(a, b):
     return
-func_name_and_time(8)
+func_name_and_time(1, 2)
